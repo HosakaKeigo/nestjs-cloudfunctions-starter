75722766687a1edade2fd5e48dd5e6b2d6f63695
HosakaKeigo <gomagoma313@gmail.com>
2024-08-01 17:52:40 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nest.js Cloud Functions Starter
+Minimal starter template for serverless Nest.js App.
+
+Deployable to Google Cloud Functions 2nd gen.
+
+# Setup
+Following will serve hello-world api on `http://localhost:8080/`
+```
+$cd functions
+$pnpm install
+$pnpm watch
+```
+
+## Hot reload
+- nodemon
+- concurrently
+- wait-on
+  - prevent `start` command before build completion.
+
+# License
+MIT licensed.
---
+++ functions/src/main.ts
@@ -0,0 +1,52 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import * as express from 'express';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as functions from '@google-cloud/functions-framework';
+
+const server = express();
+let isApplicationReady = false;
+
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.use(helmet());
+  app.enableCors({
+    origin: ['*'],
+  });
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Documentation
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js Cloud Function Starter')
+    .setDescription('A Minimal Nest.js Cloud Function Starter')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  console.log('the server is starting @ firebase');
+  return app.init();
+};
+
+const applicationReady = async () => {
+  try {
+    await createNestServer(server);
+    isApplicationReady = true;
+  } catch {
+    (err) => console.error('Nest broken', err);
+  }
+};
+
+functions.http('nestjs-cloud-function-starter', async (...args) => {
+  if (!isApplicationReady) {
+    await applicationReady();
+  }
+  server(...args);
+});
